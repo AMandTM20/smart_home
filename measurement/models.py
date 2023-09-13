df1332d665b3250591c92b61ad15430b5f5cc122
Александр Мамонтов <mamtn1957@mail.ru>
2023-09-13 23:18:08 +0300

    Initial commit

---
+++ measurement/models.py
@@ -0,0 +1,31 @@
+#from django.db import models
+
+# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+from django.db import models
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Датчик')
+    description = models.CharField(max_length=200, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    temperature = models.FloatField(verbose_name='Температура')
+    measurement_time = models.DateTimeField(auto_now_add=True, verbose_name='Время измерения')
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurement')
+
+    class Meta:
+        verbose_name = 'Измерение'
+        verbose_name_plural = 'Измерения'
+
+    def __str__(self):
+        return self.name
+
+
---
+++ measurement/views.py
@@ -0,0 +1,4 @@
+# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+# опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
+# ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
